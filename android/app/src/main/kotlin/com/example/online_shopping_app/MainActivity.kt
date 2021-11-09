506af865ec60ac6d07060ee711fc1cef275a2478
Muhammad-Bilal300 <bilalaarbi300@gmail.com>
2021-11-09 23:46:46 +0500

    Online_Shopping_App

---
+++ android/app/src/main/kotlin/com/example/online_shopping_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.online_shopping_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:online_shopping_app/constants.dart';
+import 'package:online_shopping_app/screens/home/homescreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
